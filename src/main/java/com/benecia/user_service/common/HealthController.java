18a21e995fa4d220e92d136311f516189e65fabd
Donghyeok Lim <jh07050@gmail.com>
2025-09-21 21:20:04 +0900

    feat: Health API Regsion Filter Test

--- src/main/java/com/benecia/user_service/common/HealthController.java
+++ src/main/java/com/benecia/user_service/common/HealthController.java
@@ -2,6 +2,7 @@ package com.benecia.user_service.common;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,7 +12,8 @@ public class HealthController {
     private String message;
 
     @GetMapping("/health")
-    public String healthCheck() {
+    public String healthCheck(@RequestHeader(value = "X-Custom-Header", required = false) String customHeader) {
+        System.out.println("Custom Header: " + customHeader); // 헤더 값 출력
         return message;
     }
 
