15433e0236a9e8fd8b1ebf8972b5da44d10e5c02
Donghyeok Lim <jh07050@gmail.com>
2025-11-01 21:16:26 +0900

    feat: 주문 생성 시 User 포인트 적립 메서드 구현

--- src/main/java/com/benecia/user_service/service/UserWriter.java
+++ src/main/java/com/benecia/user_service/service/UserWriter.java
@@ -1,14 +1,19 @@
 package com.benecia.user_service.service;
 
+import com.benecia.user_service.common.AppException;
+import com.benecia.user_service.common.ErrorCode;
 import com.benecia.user_service.dto.CreateUserRequest;
 import com.benecia.user_service.dto.UserResponse;
 import com.benecia.user_service.repository.UserEntity;
 import com.benecia.user_service.repository.UserJpaRepository;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class UserWriter {
@@ -16,6 +21,7 @@ public class UserWriter {
     private final UserJpaRepository userJpaRepository;
     private final PasswordEncoder passwordEncoder;
 
+    @Transactional
     public UserResponse createUser(CreateUserRequest request) {
         UserEntity userEntity = new UserEntity(
                 request.email(),
@@ -26,5 +32,15 @@ public class UserWriter {
         userJpaRepository.save(userEntity);
 
         return new UserResponse(userEntity.getUserId(), userEntity.getName(), userEntity.getEmail());
+    }
+
+    @Transactional
+    public void addPoints(String userId, int pointsToAdd) {
+        UserEntity userEntity = userJpaRepository.findByUserId(userId)
+                .orElseThrow(() -> new AppException(ErrorCode.USER_NOT_FOUND, "User not found for userId: " + userId));
+
+        userEntity.addPoints(pointsToAdd);
+        userJpaRepository.save(userEntity);
+        log.info("Points added successfully for user: {}. Total points: {}", userId, userEntity.getPoints());
     }
 }
